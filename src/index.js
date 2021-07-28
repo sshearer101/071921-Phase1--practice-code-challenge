2360d8560fac3a118bbc2f3e7ae8e0bf6e85a9e3
Sam Shearer <sshearer101@gmail.com>
2021-07-28 12:17:38 -0600

    submission for deadline

---
+++ src/index.js
@@ -0,0 +1,18 @@
+fetch ("http://localhost:3000/pups")
+.then (resp => resp.json())
+.then (data => pupName (data))
+
+
+function pupName (){
+    dogBar = document.getElementById("dog-bar"); 
+    dogBar.forEach()  
+}
+
+function createSpan(pupName){
+    const x = document.createElement('span')
+
+}
+
+
+
+
